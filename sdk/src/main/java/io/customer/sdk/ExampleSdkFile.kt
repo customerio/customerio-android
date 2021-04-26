cd2f61e4972c320a1c9c73a49c5ef2c40d5fc3e9
Levi Bostian <levi@customer.io>
2021-04-26 15:32:04 -0500

    ci: setup publish to github packages

---
+++ sdk/src/main/java/io/customer/sdk/ExampleSdkFile.kt
@@ -0,0 +1,24 @@
+package io.customer.sdk
+
+/**
+ * Example class to get the SDK setup!
+ */
+class ExampleSdkFile {
+
+    /**
+     * A variable. There you go.
+     */
+    val exampleVariable: String = "foo"
+
+    /**
+     * Adds 2 numbers together. Pretty easy.
+     *
+     * @param num1 the first number
+     * @param num2 the second number
+     *
+     * @return the sum of the 2 numbers
+     */
+    fun add(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+}

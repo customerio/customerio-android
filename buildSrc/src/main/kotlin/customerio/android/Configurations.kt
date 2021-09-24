491d4b55ecfebbbba298cd32b0cd0b27fc50b7a8
Shahroz Khan <shrozali@gmail.com>
2021-09-24 05:41:17 +0500

    chore: updated dependencies management

---
+++ buildSrc/src/main/kotlin/customerio/android/Configurations.kt
@@ -0,0 +1,8 @@
+package customerio.android
+
+object Configurations {
+    const val compileSdk = 31
+    const val targetSdk = 31
+    const val minSdk = 21
+    const val artifactGroup = "io.customer.android"
+}
---
+++ buildSrc/src/main/kotlin/customerio/android/Versions.kt
@@ -0,0 +1,17 @@
+package customerio.android
+
+object Versions {
+    internal const val ANDROID_GRADLE_PLUGIN = "7.0.2"
+    internal const val ANDROID_JUNIT5_GRADLE_PLUGIN = "1.7.1.1"
+    internal const val ANDROIDX_TEST_JUNIT = "1.1.3"
+    internal const val ANDROIDX_APPCOMPAT = "1.3.1"
+    internal const val ANDROIDX_KTX = "1.6.0"
+    internal const val COROUTINES = "1.5.1"
+    internal const val DOKKA = "1.5.0"
+    internal const val JUNIT4 = "4.13.2"
+    internal const val ESPRESSO = "3.4.0"
+    internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.1.0"
+    internal const val GRADLE_VERSIONS_PLUGIN = "0.39.0"
+    const val KOTLIN = "1.5.21"
+    internal const val TIMBER = "5.0.0"
+}

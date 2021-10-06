021d6c1691712d82cc9702212224b6b64ea7f28c
Shahroz Khan <shrozali@gmail.com>
2021-10-07 00:52:30 +0500

    Local testing sample application updated

--- app/src/main/java/io/customer/example/MainActivity.kt
+++ app/src/main/java/io/customer/example/MainActivity.kt
@@ -7,22 +7,43 @@ import io.customer.base.data.ErrorResult
 import io.customer.base.data.Success
 import io.customer.sdk.CustomerIo
 import io.customer.sdk.data.model.IdentityAttributeValue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // there are two ways to identify customer
 
+        // 1st way
+        makeSynchronousRequest()
+
+        // 2nd way
+        makeAsynchronousRequest()
+    }
+
+    private fun makeAsynchronousRequest() {
         CustomerIo.instance()
             .identify(
-                "188",
-                attributes = mapOf("email" to IdentityAttributeValue.StringAttribute("poqwrol@email.com"))
+                "identifier",
+                attributes = mapOf("email" to IdentityAttributeValue.StringAttribute("sample@email.com"))
             ).enqueue {
                 when (it) {
                     is ErrorResult -> Log.v("ErrorResult", it.error.cause.toString())
-                    is Success -> Log.v("Success", "YAAYYYYY")
+                    is Success -> Log.v("Success", "Success")
                 }
             }
-        setContentView(R.layout.activity_main)
+    }
+
+    private fun makeSynchronousRequest() {
+        CoroutineScope(Dispatchers.IO).launch {
+            when (val result = CustomerIo.instance().identify("testcoo@email.com").execute()) {
+                is ErrorResult -> Log.v("ErrorResult", result.error.cause.toString())
+                is Success -> Log.v("Success", "Success")
+            }
+        }
     }
 }

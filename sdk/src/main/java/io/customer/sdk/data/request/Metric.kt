767e2814f692e4d243fe05a13374e0f738853907
Shahroz Khan <shrozali@gmail.com>
2021-10-30 03:03:59 +0500

    Visibility modifier fixed

--- sdk/src/main/java/io/customer/sdk/data/request/Metric.kt
+++ sdk/src/main/java/io/customer/sdk/data/request/Metric.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-internal enum class MetricEvent {
+enum class MetricEvent {
     delivered, opened, converted;
 }
 

ac8636c56ba04956e3438aa568d0739edadc29fe
Shahroz Khan <shrozali@gmail.com>
2022-01-14 07:17:13 +0500

    updated track endpoints

--- sdk/src/main/java/io/customer/sdk/data/model/Region.kt
+++ sdk/src/main/java/io/customer/sdk/data/model/Region.kt
@@ -5,6 +5,6 @@ package io.customer.sdk.data.model
  * The SDK will route traffic to the correct data center location depending on the `Region` that you use.
  */
 sealed class Region(val code: String, val baseUrl: String) {
-    object US : Region(code = "us", baseUrl = "https://track.customer.io/")
-    object EU : Region(code = "eu", baseUrl = "https://track-eu.customer.io/")
+    object US : Region(code = "us", baseUrl = "https://track-sdk.customer.io/")
+    object EU : Region(code = "eu", baseUrl = "https://track-sdk-eu.customer.io/")
 }

736a126071e31e463925d3c22e7f292181144d63
Shahroz Khan <shrozali@gmail.com>
2022-01-14 21:53:47 +0500

    Added comments for hostname

--- sdk/src/main/java/io/customer/sdk/data/model/Region.kt
+++ sdk/src/main/java/io/customer/sdk/data/model/Region.kt
@@ -5,6 +5,10 @@ package io.customer.sdk.data.model
  * The SDK will route traffic to the correct data center location depending on the `Region` that you use.
  */
 sealed class Region(val code: String, val baseUrl: String) {
+
+    // Note: These URLs are meant to be used specifically by the official
+    // mobile SDKs. View our API docs: https://customer.io/docs/api/
+    // to find the correct hostname for what you're trying to do.
     object US : Region(code = "us", baseUrl = "https://track-sdk.customer.io/")
     object EU : Region(code = "eu", baseUrl = "https://track-sdk-eu.customer.io/")
 }

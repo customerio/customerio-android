58c791b8a6c9df91506cf75064622a399ca9a8d0
Shahroz Khan <shrozali@gmail.com>
2022-01-21 19:39:11 +0500

    fix: added support for big decimal (#56)

---
+++ sdk/src/main/java/io/customer/sdk/data/moshi/adapter/BigDecimalAdapter.kt
@@ -0,0 +1,13 @@
+package io.customer.sdk.data.moshi.adapter
+
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import java.math.BigDecimal
+
+class BigDecimalAdapter {
+    @FromJson
+    fun fromJson(string: String) = BigDecimal(string)
+
+    @ToJson
+    fun toJson(value: BigDecimal) = value.toString()
+}

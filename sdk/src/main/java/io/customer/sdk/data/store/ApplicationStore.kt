730e75f16d3569ddf27b021305205622974df81c
Levi Bostian <levi@customer.io>
2022-03-21 15:55:02 -0500

    change to using package name in user-agent instead of app name

--- sdk/src/main/java/io/customer/sdk/data/store/ApplicationStore.kt
+++ sdk/src/main/java/io/customer/sdk/data/store/ApplicationStore.kt
@@ -4,30 +4,29 @@ import android.content.Context
 
 interface ApplicationStore {
     // Customer App information
-    val customerAppName: String
-    val customerAppVersion: String
+    val customerAppName: String?
+    val customerAppVersion: String?
+    val customerPackageName: String
 }
 
 internal class ApplicationStoreImp(val context: Context) : ApplicationStore {
 
-    private val appInfo: Pair<String, String> = getAppInformation()
-
-    override val customerAppName: String
-        get() = appInfo.first
-    override val customerAppVersion: String
-        get() = appInfo.second
-
-    private fun getAppInformation(): Pair<String, String> {
-        val appName: String = try {
+    override val customerAppName: String?
+        get() = tryGetValueOrNull {
             context.applicationInfo.loadLabel(context.packageManager).toString()
-        } catch (e: Exception) {
-            ""
         }
-        val appVersion: String = try {
+    override val customerAppVersion: String?
+        get() = tryGetValueOrNull {
             context.packageManager.getPackageInfo(context.packageName, 0).versionName
-        } catch (e: Exception) {
-            ""
         }
-        return appName to appVersion
+    override val customerPackageName: String
+        get() = context.packageName
+
+    private fun tryGetValueOrNull(tryGetValue: () -> String): String? {
+        return try {
+            tryGetValue()
+        } catch (e: Exception) {
+            null
+        }
     }
 }

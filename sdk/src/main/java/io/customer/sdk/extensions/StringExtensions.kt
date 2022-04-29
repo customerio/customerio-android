a99cc3f74209b0640c0ecd0abc9c156c85b49939
Shahroz Khan <shrozali@gmail.com>
2022-04-29 16:58:56 +0500

    added alternative route for activity name

---
+++ sdk/src/main/java/io/customer/sdk/extensions/StringExtensions.kt
@@ -0,0 +1,9 @@
+package io.customer.sdk.extensions
+
+fun String.getScreenNameFromActivity(): String {
+    val regex = Regex(
+        pattern = "Activity|ListActivity|FragmentActivity|DialogActivity",
+        option = RegexOption.IGNORE_CASE
+    )
+    return this.replace(regex, "")
+}

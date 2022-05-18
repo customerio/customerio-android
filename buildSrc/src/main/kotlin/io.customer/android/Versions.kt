0ba9305076e582a7f6629d05f984f1ef111f1efc
Shahroz Khan <shrozali@gmail.com>
2022-05-18 21:10:49 +0500

    added base in-app module

--- buildSrc/src/main/kotlin/io.customer/android/Versions.kt
+++ buildSrc/src/main/kotlin/io.customer/android/Versions.kt
@@ -1,7 +1,7 @@
 package io.customer.android
 
 object Versions {
-    internal const val ANDROID_GRADLE_PLUGIN = "7.0.3"
+    internal const val ANDROID_GRADLE_PLUGIN = "7.2.0"
     internal const val ANDROIDX_TEST_JUNIT = "1.1.3"
     internal const val ANDROIDX_TEST_RUNNER = "1.4.0"
     internal const val ANDROIDX_TEST_RULES = "1.4.0"

0e24769dd36cf2f12aa26951a074d3c3050e8422
Levi Bostian <levi@customer.io>
2022-06-13 10:58:02 -0500

    add more notes [skip ci]

--- sdk/src/main/java/io/customer/sdk/util/Logger.kt
+++ sdk/src/main/java/io/customer/sdk/util/Logger.kt
@@ -68,6 +68,14 @@ class LogcatLogger(
         // many use cases where an exception can be thrown and crash the app. This code is not critical to the functionality
         // of the SDK so we wrap all of it's logic in a try/catch to prevent crashing the app.
         try {
+            // Attempt to write to Downloads folder on the Android device. This allows customers/QA to send us logs really easily
+            // by navigating to their Downloads folder on their Android device and sharing the file. This is unlike saving the log
+            // file to internal storage of the mobile app where it's difficult to access.
+
+            // `getExternalStoragePublicDirectory` is deprecated but from my testing, it is still working.
+            // There are new ways to save to Downloads folder on Android but it requires pop-ups to select save locations
+            // and give permissions. This can be confusing for a customer for a pop-up asking for Downloads permission for use
+            // of our SDK. We can figure out what to do if we find this method below does not work anymore.
             val target = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
             // Create a new log file for each day to prevent creatig 1 file that is really long.
             val logFile = File(target, "customerio-sdk-logs-${Date().toString(DateFormat.DATE_NO_TIME)}.txt")

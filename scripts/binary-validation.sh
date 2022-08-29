9b290e7e4a853fa7469c6c00b718149be1403193
Shahroz Khan <shrozali@gmail.com>
2022-08-29 17:15:17 +0500

    merged conflicts resolution

--- scripts/binary-validation.sh
+++ scripts/binary-validation.sh
@@ -3,11 +3,12 @@
 ./gradlew apiCheck -q
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
-  echo "❌ apiCheck failed, running apiDump for you..."
+  # shellcheck disable=SC2028
+  echo "❌ Looks like some 'public' mode was added or removed from the project. If we're not careful, this can break the SDK for customers.\n\nGenerating new .api files for you to add to your commit. Make sure to review these files in your pull request to make sure your modifications were not on purpose."
 
   ./gradlew apiDump -q
 
-  echo "API dump done, please check the results and then try your commit again!"
+  echo "API dump done, please check the modified '.api' files to make sure that the 'public' code changes that were done were done on purpose and not by accident. If done on purpose, please try committing again!"
   exit $EXIT_CODE
 fi
 

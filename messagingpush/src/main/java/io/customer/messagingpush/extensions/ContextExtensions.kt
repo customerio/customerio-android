54625f1b8fbf6d7c12143f46b994c59f4ecdc6ee
Rehan <mrehan.official@gmail.com>
2022-09-19 19:09:05 +0500

    feat: added support to modify notification small icon

---
+++ messagingpush/src/main/java/io/customer/messagingpush/extensions/ContextExtensions.kt
@@ -0,0 +1,25 @@
+package io.customer.messagingpush.extensions
+
+import android.content.Context
+import android.content.res.Resources
+import android.os.Build
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import io.customer.sdk.CustomerIO
+
+@DrawableRes
+internal fun Context.getDrawableByName(name: String?): Int? = if (name.isNullOrBlank()) null
+else resources?.getIdentifier(name, "drawable", packageName)?.takeUnless { id ->
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) id == Resources.ID_NULL
+    else id == 0
+}
+
+@ColorInt
+internal fun Context.getColorOrNull(@ColorRes id: Int): Int? = try {
+    ContextCompat.getColor(this, id)
+} catch (ex: Resources.NotFoundException) {
+    CustomerIO.instance().diGraph.logger.error("Invalid resource $id, ${ex.message}")
+    null
+}

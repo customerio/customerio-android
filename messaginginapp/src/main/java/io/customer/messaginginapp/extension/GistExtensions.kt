6910c397c4f850aa44ecac2a96d1a2a633c0b6f0
Levi Bostian <levi@customer.io>
2022-11-14 23:55:19 -0600

    add tests

---
+++ messaginginapp/src/main/java/io/customer/messaginginapp/extension/GistExtensions.kt
@@ -0,0 +1,22 @@
+package io.customer.messaginginapp.extension
+
+import build.gist.data.model.Message
+import io.customer.messaginginapp.type.InAppMessage
+import io.customer.sdk.extensions.random
+
+fun Message.getNewFromInApp(
+    message: InAppMessage
+): Message = Message(
+    instanceId = message.instanceId,
+    messageId = message.messageId,
+    properties = mapOf(
+        Pair(
+            "gist",
+            mapOf(
+                Pair("campaignId", message.deliveryId)
+            )
+        )
+    )
+)
+
+fun Message.getNewRandom(): Message = getNewFromInApp(InAppMessage(String.random, String.random, String.random))

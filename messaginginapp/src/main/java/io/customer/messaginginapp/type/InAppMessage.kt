f34e991e4feae15b4823206b0dd77a48e284ab94
Levi Bostian <levi@customer.io>
2022-11-15 23:19:06 -0600

    make deliveryid optional

--- messaginginapp/src/main/java/io/customer/messaginginapp/type/InAppMessage.kt
+++ messaginginapp/src/main/java/io/customer/messaginginapp/type/InAppMessage.kt
@@ -6,14 +6,12 @@ import build.gist.data.model.Message
 data class InAppMessage(
     val instanceId: String,
     val messageId: String,
-    val deliveryId: String // (Currently taken from Gist's campaignId property)
+    val deliveryId: String? // (Currently taken from Gist's campaignId property). Can be null when sending test in-app messages
 ) {
     companion object {
         internal fun getFromGistMessage(gistMessage: Message): InAppMessage {
-            // From viewing the Gist SDK source code, looks like the campaign id of a message is always populated.
-            // Here is where Messages are created: https://gitlab.com/bourbonltd/gist-android/-/blob/467deb23be88fda089d0f3d85e23b3b1ba0f91fe/gist/src/main/java/build/gist/presentation/GistView.kt#L86
             val gistProperties = GistMessageProperties.getGistProperties(gistMessage)
-            val campaignId = gistProperties.campaignId!!
+            val campaignId = gistProperties.campaignId
 
             return InAppMessage(
                 instanceId = gistMessage.instanceId,

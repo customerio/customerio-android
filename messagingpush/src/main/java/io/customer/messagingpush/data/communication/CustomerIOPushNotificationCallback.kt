96fea17c0b88b176e1a0bf4454c47f5f9074cc01
Shahroz Khan <shrozali@gmail.com>
2023-03-26 22:39:06 +0500

    expose a callback to override deeplink intent

--- messagingpush/src/main/java/io/customer/messagingpush/data/communication/CustomerIOPushNotificationCallback.kt
+++ messagingpush/src/main/java/io/customer/messagingpush/data/communication/CustomerIOPushNotificationCallback.kt
@@ -3,7 +3,6 @@ package io.customer.messagingpush.data.communication
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
 import io.customer.messagingpush.data.model.CustomerIOParsedPushPayload
@@ -46,33 +45,15 @@ interface CustomerIOPushNotificationCallback {
     ) = Unit
 
     /**
-     * Called before setting flags for deep link intent. This is helpful in preventing
-     * recreation of activities and handling more launch modes to achieve desired behavior.
+     * Called before setting flags for deep link intent. This is helpful in providing
+     * custom flags for the intent that is about to be opened for the provided deep link.
      *
      * @param intent intent about to open for the provided link.
      * @param uri deep link uri received in the notification.
-     * @param startingFromService flag to indicate whether the intent is launching intent from
-     * service or not. This will be true only in Android 12 and later for links outside the host
-     * app due to notification trampoline restrictions.
-     * @param isThirdPartyIntent flag to indicate whether the intent being launched is outside the
-     * the host app or not, true for non-host app intents, false otherwise.
      * @return flags to be set for the intent; null to don't set any flags.
      */
     fun getDeepLinkIntentFlags(
         intent: Intent,
-        uri: Uri,
-        startingFromService: Boolean,
-        isThirdPartyIntent: Boolean
-    ): Int? {
-        val defaultFlags = Intent.FLAG_ACTIVITY_NEW_TASK or
-            Intent.FLAG_ACTIVITY_CLEAR_TOP or
-            Intent.FLAG_ACTIVITY_SINGLE_TOP
-
-        return when {
-            !startingFromService -> null
-            !isThirdPartyIntent && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
-            -> defaultFlags or Intent.FLAG_ACTIVITY_REQUIRE_NON_BROWSER
-            else -> defaultFlags
-        }
-    }
+        uri: Uri
+    ): Int? = null
 }

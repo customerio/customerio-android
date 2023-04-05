96bbf0792e83fd9122827ede7096756249d232f8
Rehan <mrehan.official@gmail.com>
2023-04-05 17:38:37 +0500

    exposed method for requesting missing events

--- sdk/src/main/java/io/customer/sdk/CustomerIOActivityLifecycleCallbacks.kt
+++ sdk/src/main/java/io/customer/sdk/CustomerIOActivityLifecycleCallbacks.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.app.Application.ActivityLifecycleCallbacks
 import android.os.Bundle
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import io.customer.base.internal.InternalCustomerIOApi
 import io.customer.sdk.lifecycle.LifecycleCallback
 
 class CustomerIOActivityLifecycleCallbacks internal constructor(
@@ -15,6 +17,45 @@ class CustomerIOActivityLifecycleCallbacks internal constructor(
     fun registerCallback(callback: LifecycleCallback) {
         for (event in callback.eventsToObserve) {
             eventCallbacks.getOrPut(event) { mutableListOf() }.add(callback)
+        }
+    }
+
+    /**
+     * In non-native/wrapper SDKs, module listeners are attached after the
+     * activity has been created which results in missing initial lifecycle
+     * events. This method helps completing any pending actions by emitting
+     * those events manually. This method should be called as soon as SDK is
+     * initialized in wrapper SDKs by client app.
+     */
+    @InternalCustomerIOApi
+    fun postDelayedEventsForNonNativeActivity(activity: Activity) {
+        val lifecycleAwareActivity = activity as? LifecycleOwner ?: return
+
+        val currentState = lifecycleAwareActivity.lifecycle.currentState
+        val bundle = activity.intent?.extras
+        val pendingEvents = mutableListOf<Lifecycle.Event>()
+
+        when {
+            currentState.isAtLeast(Lifecycle.State.RESUMED) -> {
+                pendingEvents.add(Lifecycle.Event.ON_CREATE)
+                pendingEvents.add(Lifecycle.Event.ON_START)
+                pendingEvents.add(Lifecycle.Event.ON_RESUME)
+            }
+            currentState.isAtLeast(Lifecycle.State.STARTED) -> {
+                pendingEvents.add(Lifecycle.Event.ON_CREATE)
+                pendingEvents.add(Lifecycle.Event.ON_START)
+            }
+            currentState.isAtLeast(Lifecycle.State.CREATED) -> {
+                pendingEvents.add(Lifecycle.Event.ON_CREATE)
+            }
+        }
+
+        for (event in pendingEvents) {
+            sendEventToCallbacks(
+                event = event,
+                activity = activity,
+                bundle = bundle
+            )
         }
     }
 

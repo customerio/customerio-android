4cee3b3d02a01f465e82173a33fc8f0f7ea52acb
Rehan <mrehan.official@gmail.com>
2023-05-02 17:23:57 +0500

    updated base classes

--- samples/java_layout/src/main/java/io/customer/android/sample/java_layout/SampleApplication.java
+++ samples/java_layout/src/main/java/io/customer/android/sample/java_layout/SampleApplication.java
@@ -2,5 +2,17 @@ package io.customer.android.sample.java_layout;
 
 import android.app.Application;
 
+import io.customer.android.sample.java_layout.di.ApplicationGraph;
+
 public class SampleApplication extends Application {
+    private final ApplicationGraph appGraph = new ApplicationGraph(this);
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public ApplicationGraph getApplicationGraph() {
+        return appGraph;
+    }
 }

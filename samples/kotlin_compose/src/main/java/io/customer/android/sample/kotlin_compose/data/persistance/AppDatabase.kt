86fc387db514f48fe21f25c1e62604b1b76ced37
Shahroz Khan <shrozali@gmail.com>
2023-05-02 16:45:07 +0500

    added db for user

---
+++ samples/kotlin_compose/src/main/java/io/customer/android/sample/kotlin_compose/data/persistance/AppDatabase.kt
@@ -0,0 +1,30 @@
+package io.customer.android.sample.kotlin_compose.data.persistance
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import io.customer.android.sample.kotlin_compose.R
+import io.customer.android.sample.kotlin_compose.data.models.User
+
+@Database(entities = [User::class], version = 1, exportSchema = true)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase = INSTANCE ?: synchronized(this) {
+            INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            context.getString(R.string.database)
+        ).fallbackToDestructiveMigration().build()
+    }
+}

72f8d4ea6b98d1bbbb83f80dfebf4a859cb9a12b
Rehan <mrehan.official@gmail.com>
2023-05-02 21:15:53 +0500

    minor fixes

--- samples/java_layout/src/main/java/io/customer/android/sample/java_layout/core/Constants.java
+++ samples/java_layout/src/main/java/io/customer/android/sample/java_layout/core/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
         public static class CustomerIOSDK {
             public static final String API_KEY = "cio_sdk_api_key";
             public static final String SITE_ID = "cio_sdk_site_id";
-            public static final String TRACKING_API_URL = "cio_sdk_site_id";
+            public static final String TRACKING_API_URL = "cio_sdk_tracking_api_url";
         }
     }
 }
--- samples/java_layout/src/main/java/io/customer/android/sample/java_layout/support/Optional.java
+++ samples/java_layout/src/main/java/io/customer/android/sample/java_layout/support/Optional.java
@@ -3,6 +3,7 @@ package io.customer.android.sample.java_layout.support;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+// Can be replaced with [java.util.Optional] once minSdk version is increased to 24
 public interface Optional<T> {
     static <T> Optional<T> empty() {
         return new OptionalImpl<>(null);

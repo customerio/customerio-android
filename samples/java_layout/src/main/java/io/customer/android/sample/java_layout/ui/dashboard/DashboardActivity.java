a72b844920753abbddba1b2e1a3f48c311a23e05
Rehan <mrehan.official@gmail.com>
2023-05-04 12:15:48 +0500

    chore: java sample dashboard screen

--- samples/java_layout/src/main/java/io/customer/android/sample/java_layout/ui/dashboard/DashboardActivity.java
+++ samples/java_layout/src/main/java/io/customer/android/sample/java_layout/ui/dashboard/DashboardActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.view.View;
 import android.view.ViewTreeObserver;
 
+import io.customer.android.sample.java_layout.R;
 import io.customer.android.sample.java_layout.databinding.ActivityDashboardBinding;
 import io.customer.android.sample.java_layout.ui.core.BaseActivity;
 import io.customer.android.sample.java_layout.ui.login.LoginActivity;
+import io.customer.android.sample.java_layout.ui.settings.SettingsActivity;
 import io.customer.android.sample.java_layout.ui.user.AuthViewModel;
 
 public class DashboardActivity extends BaseActivity<ActivityDashboardBinding> {
@@ -43,6 +45,9 @@ public class DashboardActivity extends BaseActivity<ActivityDashboardBinding> {
                         }
 
                         content.getViewTreeObserver().removeOnPreDrawListener(this);
+                        if (isLoggedIn) {
+                            binding.progressIndicator.hide();
+                        }
                         return true;
                     }
                 }
@@ -50,18 +55,28 @@ public class DashboardActivity extends BaseActivity<ActivityDashboardBinding> {
     }
 
     private void setupViews() {
+        binding.settingsButton.setOnClickListener(view -> {
+            startActivity(new Intent(DashboardActivity.this, SettingsActivity.class));
+        });
         binding.logoutButton.setOnClickListener(view -> {
             authViewModel.clearLoggedInUser();
         });
     }
 
     private void setupObservers() {
+        // TODO: Fetch and set user agent here
+        binding.userAgentTextView.setText("User agent will be shown here");
         authViewModel.getUserLoggedInStateObservable().observe(this, isLoggedIn -> {
         });
         authViewModel.getUserDataObservable().observe(this, user -> {
+            binding.greetingsTextView.setText(
+                    getString(R.string.dashboard_greeting_message_format, user.getDisplayName())
+            );
         });
         authViewModel.getUserLoggedInStateObservable().observe(this, isLoggedIn -> {
-            if (!isLoggedIn) {
+            if (isLoggedIn) {
+                binding.content.setVisibility(View.VISIBLE);
+            } else {
                 startActivity(new Intent(DashboardActivity.this, LoginActivity.class));
                 finish();
             }

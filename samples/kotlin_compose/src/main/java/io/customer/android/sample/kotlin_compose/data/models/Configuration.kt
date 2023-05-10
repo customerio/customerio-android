832b798835c55eca0881ba85f72981fd8a9f10de
Shahroz Khan <shrozali@gmail.com>
2023-05-10 14:18:19 +0500

    added sdk constants for default value

--- samples/kotlin_compose/src/main/java/io/customer/android/sample/kotlin_compose/data/models/Configuration.kt
+++ samples/kotlin_compose/src/main/java/io/customer/android/sample/kotlin_compose/data/models/Configuration.kt
@@ -1,15 +1,20 @@
 package io.customer.android.sample.kotlin_compose.data.models
 
+import io.customer.sdk.CustomerIOConfig.Companion.AnalyticsConstants.AUTO_TRACK_DEVICE_ATTRIBUTES
+import io.customer.sdk.CustomerIOConfig.Companion.AnalyticsConstants.BACKGROUND_QUEUE_MIN_NUMBER_OF_TASKS
+import io.customer.sdk.CustomerIOConfig.Companion.AnalyticsConstants.BACKGROUND_QUEUE_SECONDS_DELAY
+import io.customer.sdk.CustomerIOConfig.Companion.AnalyticsConstants.SHOULD_AUTO_RECORD_SCREEN_VIEWS
+
 data class Configuration(
     var siteId: String,
     var apiKey: String
 ) {
     var trackUrl: String? = null
 
-    var backgroundQueueSecondsDelay: Double = 30.0
-    var backgroundQueueMinNumTasks: Int = 10
+    var backgroundQueueSecondsDelay: Double = BACKGROUND_QUEUE_SECONDS_DELAY
+    var backgroundQueueMinNumTasks: Int = BACKGROUND_QUEUE_MIN_NUMBER_OF_TASKS
 
-    var trackScreen: Boolean = false
-    var trackDeviceAttributes: Boolean = false
+    var trackScreen: Boolean = SHOULD_AUTO_RECORD_SCREEN_VIEWS
+    var trackDeviceAttributes: Boolean = AUTO_TRACK_DEVICE_ATTRIBUTES
     var debugMode: Boolean = false
 }

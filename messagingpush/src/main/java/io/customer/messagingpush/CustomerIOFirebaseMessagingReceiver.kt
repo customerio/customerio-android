0a9c4571e93f9ce94f312b58dbb0975d0d9bd448
Rehan <mrehan.official@gmail.com>
2023-05-12 15:08:05 +0500

    fix: improve delivered metrics

---
+++ messagingpush/src/main/java/io/customer/messagingpush/CustomerIOFirebaseMessagingReceiver.kt
@@ -0,0 +1,11 @@
+package io.customer.messagingpush
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class CustomerIOFirebaseMessagingReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // Handle receive event here
+    }
+}

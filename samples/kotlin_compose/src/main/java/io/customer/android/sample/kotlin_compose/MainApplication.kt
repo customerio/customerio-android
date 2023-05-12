d342be4271045c686dd5c5fff286c4ced0471cd8
Shahroz Khan <shrozali@gmail.com>
2023-05-12 18:56:48 +0500

    Revert "encapsulated the configurations"
    This reverts commit b414df8c522bd9e109faeb059460142769ccc449.

--- samples/kotlin_compose/src/main/java/io/customer/android/sample/kotlin_compose/MainApplication.kt
+++ samples/kotlin_compose/src/main/java/io/customer/android/sample/kotlin_compose/MainApplication.kt
@@ -2,11 +2,11 @@ package io.customer.android.sample.kotlin_compose
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import io.customer.android.sample.kotlin_compose.data.models.setValuesFromBuilder
 import io.customer.android.sample.kotlin_compose.data.repositories.PreferenceRepository
 import io.customer.messaginginapp.ModuleMessagingInApp
 import io.customer.messagingpush.ModuleMessagingPushFCM
 import io.customer.sdk.CustomerIO
+import io.customer.sdk.util.CioLogLevel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
@@ -28,7 +28,18 @@ class MainApplication : Application() {
             apiKey = configuration.apiKey,
             appContext = this
         ).apply {
-            configuration.setValuesFromBuilder(this)
+            configuration.trackUrl?.let {
+                setTrackingApiURL(trackingApiUrl = it)
+            }
+            setBackgroundQueueSecondsDelay(configuration.backgroundQueueSecondsDelay)
+            setBackgroundQueueMinNumberOfTasks(configuration.backgroundQueueMinNumTasks)
+            if (configuration.debugMode) {
+                setLogLevel(CioLogLevel.DEBUG)
+            } else {
+                setLogLevel(CioLogLevel.ERROR)
+            }
+            autoTrackDeviceAttributes(configuration.trackDeviceAttributes)
+            autoTrackScreenViews(configuration.trackScreen)
 
             addCustomerIOModule(ModuleMessagingInApp())
             addCustomerIOModule(ModuleMessagingPushFCM())

a7b9d73b5d95e64285e5db83cee03c91e8dacd87
Shahroz Khan <shrozali@gmail.com>
2023-05-16 12:52:02 +0500

    chore: added dashboard and custom event tracking (#205)

--- samples/kotlin_compose/src/main/java/io/customer/android/sample/kotlin_compose/ui/customTrack/CustomAttribute.kt
+++ samples/kotlin_compose/src/main/java/io/customer/android/sample/kotlin_compose/ui/customTrack/CustomAttribute.kt
@@ -1,9 +1,102 @@
 package io.customer.android.sample.kotlin_compose.ui.customTrack
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import io.customer.android.sample.kotlin_compose.R
+import io.customer.android.sample.kotlin_compose.navigation.Screen.CustomAttribute.TYPE_PROFILE
+import io.customer.android.sample.kotlin_compose.ui.components.ActionButton
+import io.customer.android.sample.kotlin_compose.ui.components.HeaderText
+import io.customer.sdk.CustomerIO
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomAttributeRoute(
-    onBackPressed: () -> Unit
+    onBackPressed: () -> Unit,
+    attributeType: String
 ) {
+    var attributeName by remember { mutableStateOf("") }
+    var attributeValue by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        IconButton(
+            onClick = onBackPressed
+        ) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "Back",
+                modifier = Modifier.align(Alignment.Start)
+            )
+        }
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 16.dp)
+        ) {
+            HeaderText(
+                string = if (attributeType == TYPE_PROFILE) {
+                    stringResource(R.string.set_profile_attribute)
+                } else {
+                    stringResource(R.string.set_device_attribute)
+                }
+            )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = attributeName,
+                onValueChange = {
+                    attributeName = it
+                },
+                label = {
+                    Text(text = stringResource(id = R.string.attribute_name))
+                }
+            )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = attributeValue,
+                onValueChange = {
+                    attributeValue = it
+                },
+                label = {
+                    Text(text = stringResource(id = R.string.attribute_value))
+                }
+            )
+
+            val (btnTitle, action) = if (attributeType == TYPE_PROFILE) {
+                Pair(
+                    stringResource(R.string.send_profile_attribute)
+                ) {
+                    CustomerIO.instance().profileAttributes = mapOf(attributeName to attributeValue)
+                }
+            } else {
+                Pair(stringResource(R.string.send_device_attribute)) {
+                    CustomerIO.instance().deviceAttributes = mapOf(attributeName to attributeValue)
+                }
+            }
+            ActionButton(text = btnTitle, onClick = action)
+        }
+    }
 }
--- samples/kotlin_compose/src/main/java/io/customer/android/sample/kotlin_compose/ui/dashboard/DashboardViewModel.kt
+++ samples/kotlin_compose/src/main/java/io/customer/android/sample/kotlin_compose/ui/dashboard/DashboardViewModel.kt
@@ -1,5 +1,43 @@
 package io.customer.android.sample.kotlin_compose.ui.dashboard
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.customer.android.sample.kotlin_compose.data.models.User
+import io.customer.android.sample.kotlin_compose.data.repositories.UserRepository
+import io.customer.sdk.CustomerIO
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class DashboardViewModel : ViewModel()
+@HiltViewModel
+class DashboardViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    // UI state exposed to the UI
+    private val _uiState = MutableStateFlow(User("", ""))
+    val uiState: StateFlow<User> = _uiState.asStateFlow()
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        viewModelScope.launch {
+            userRepository.getUser().collect {
+                _uiState.value = it ?: User("", "")
+            }
+        }
+    }
+
+    fun logout(user: User, onLogout: () -> Unit) {
+        viewModelScope.launch {
+            userRepository.deleteUser(user)
+            CustomerIO.instance().clearIdentify()
+            onLogout.invoke()
+        }
+    }
+}

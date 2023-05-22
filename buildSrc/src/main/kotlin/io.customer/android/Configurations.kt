d128be0f316e1a4994de383dd301815c75344efd
Shahroz Khan <shrozali@gmail.com>
2023-05-23 01:41:57 +0500

    added instrumentation testing setup

--- buildSrc/src/main/kotlin/io.customer/android/Configurations.kt
+++ buildSrc/src/main/kotlin/io.customer/android/Configurations.kt
@@ -1,7 +1,7 @@
 package io.customer.android
 
 object Configurations {
-    const val compileSdk = 30
+    const val compileSdk = 33
     const val targetSdk = 30
     const val minSdk = 21
     const val artifactGroup = "io.customer.android"

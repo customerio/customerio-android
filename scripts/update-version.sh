3b527a1db3ad7097ce23259507412b497ce45e74
Levi Bostian <levi@customer.io>
2023-05-30 09:59:19 -0500

    ci: update SDK version before building sample apps (#212)

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -7,22 +7,17 @@
 set -e
 
 NEW_VERSION="$1"
-KOTLIN_SOURCE_FILE="sdk/src/main/java/io/customer/sdk/Version.kt"
 
-# Given line: `    static let version: String = "0.1.1"`
-# Regex string will match the line of the file that we can then substitute.
-LINE_PATTERN="const val version: String = \"\(.*\)\""
+RELATIVE_PATH_TO_SCRIPTS_DIR=$(dirname "$0")
+ABSOLUTE_PATH_TO_SOURCE_CODE_ROOT_DIR=$(realpath "$RELATIVE_PATH_TO_SCRIPTS_DIR/..")
+KOTLIN_SOURCE_FILE="$ABSOLUTE_PATH_TO_SOURCE_CODE_ROOT_DIR/sdk/src/main/java/io/customer/sdk/Version.kt"
 
 echo "Updating file: $KOTLIN_SOURCE_FILE to new version: $NEW_VERSION"
 
-# -i overwrites file
-# "s/" means substitute given pattern with given string.
-#
-sed -i "s/$LINE_PATTERN/const val version: String = \"$NEW_VERSION\"/" "$KOTLIN_SOURCE_FILE"
+# Uses CLI tool sd to replace string in a file: https://github.com/chmln/sd
+# Given line: `    static let version: String = "0.1.1"`
+# Regex string will match the line of the file that we can then substitute.
+sd "const val version: String = \"(.*)\"" "const val version: String = \"$NEW_VERSION\"" $KOTLIN_SOURCE_FILE
 
-echo "Done! New version: "
-
-# print the line (/p) that is matched in the file to show the change.
-sed -n "/$LINE_PATTERN/p" $KOTLIN_SOURCE_FILE
-
-echo "\n\n Done!\n Dont forget to commit your changes. A good commit message is: \"chore: prepare for $NEW_VERSION\""
+echo "Done! Showing changes to confirm it worked: "
+git diff $KOTLIN_SOURCE_FILE

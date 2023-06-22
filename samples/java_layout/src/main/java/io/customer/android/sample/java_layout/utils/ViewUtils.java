e66ac9e5ed169744d4cf4f83baebf9eed4a06233
Rehan <mrehan.official@gmail.com>
2023-06-22 15:30:12 +0500

    appium tag helper methods

--- samples/java_layout/src/main/java/io/customer/android/sample/java_layout/utils/ViewUtils.java
+++ samples/java_layout/src/main/java/io/customer/android/sample/java_layout/utils/ViewUtils.java
@@ -2,11 +2,14 @@ package io.customer.android.sample.java_layout.utils;
 
 import android.app.Activity;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputLayout;
@@ -14,9 +17,18 @@ import com.google.android.material.textfield.TextInputLayout;
 import java.util.Locale;
 
 import io.customer.android.sample.java_layout.BuildConfig;
+import io.customer.android.sample.java_layout.R;
 import io.customer.sdk.CustomerIO;
 
 public class ViewUtils {
+    public static void prepareForAutomatedTests(@NonNull View view, @StringRes int contentDescResId) {
+        view.setContentDescription(view.getContext().getString(contentDescResId));
+    }
+
+    public static void prepareForAutomatedTests(@NonNull Toolbar toolbar) {
+        toolbar.setNavigationContentDescription(R.string.acd_back_button_icon);
+    }
+
     @NonNull
     public static String getText(@NonNull EditText editText) {
         return editText.getText().toString();

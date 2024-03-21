726667ba4380b21cdf88042fe1984596f08f82f1
Shahroz Khan <shrozali@gmail.com>
2024-03-21 21:15:41 +0500

    fix: last used format for device (#287)

--- sdk/src/main/java/io/customer/sdk/data/request/Device.kt
+++ sdk/src/main/java/io/customer/sdk/data/request/Device.kt
@@ -9,7 +9,7 @@ import java.util.*
 data class Device(
     @field:Json(name = "id") val token: String,
     val platform: String = "android",
-    val lastUsed: Date,
+    @field:Json(name = "last_used") val lastUsed: Date?, // nullable to cater for `lastUsed` field in older versions of the SDK
     val attributes: CustomAttributes
 )
 

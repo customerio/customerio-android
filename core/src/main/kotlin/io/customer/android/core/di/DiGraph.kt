f8980be317d6a3ed862680db8046debe0adfc19f
Muhammad Rehan <mrehan.official@gmail.com>
2024-04-27 00:19:00 +0500

    refactor: improve digraph support (#307)

--- core/src/main/kotlin/io/customer/android/core/di/DiGraph.kt
+++ core/src/main/kotlin/io/customer/android/core/di/DiGraph.kt
@@ -1,64 +1,117 @@
 package io.customer.android.core.di
 
 abstract class DiGraph {
+    /**
+     * Map of dependencies that can be overridden with mocks in test functions.
+     */
     val overrides: MutableMap<String, Any> = mutableMapOf()
 
     /**
-     * Call when you want to override a dependency with a mock in test functions.
-     * `di.overrideDependency(CustomerIOInstance::class.java, customerIOMock)`
-     *
-     * See `CustomerIOTest` (or any other class that is top-level of the module) for an example on use.
-     */
-    fun <DEP> overrideDependency(dependency: Class<DEP>, value: DEP) {
-        overrides[dependency.simpleName] = value as Any
-    }
-
-    /**
-     * Call in Digraph property getters to get allow mock overriding before trying to get actual instance.
-     * ```
-     * val fileStorage: FileStorage
-     *   get() = override() ?: FileStorage(siteId, context)
-     * ```
-     * Note: Don't forget to include the interface in the property declaration:
-     * ```
-     * val foo: InterfaceOfFoo
-     *          ^^^^^^^^^^^^^^
-     * ```
-     * Or you may not be able to successfully mock in test functions.
-     */
-    inline fun <reified DEP> override(): DEP? = overrides[DEP::class.java.simpleName] as? DEP
-
-    /**
-     * We prefer to have all of the SDK's singleton instances held in the dependency injection graph. This makes it easier for automated tests to be able to delete all
-     * singletons between each test function and prevent test flakiness.
+     * Map of singleton instances of dependencies in the graph.
      */
     val singletons: MutableMap<String, Any> = mutableMapOf()
 
     /**
-     * In the graph, if you have any dependency that should be a singleton:
-     * ```
-     * val queue: Queue
-     *   get() = override() ?: getSingletonInstanceCreate {
-     *     QueueImpl(...)
-     *   }
-     * ```
+     * Internal function to get the key for a dependency in the graph based on its class.
+     * This function is marked as public only because it is inlined in other public functions.
      */
-    inline fun <reified INST : Any> getSingletonInstanceCreate(newInstanceCreator: () -> INST): INST {
-        // Use a synchronized block to prevent multiple threads from creating multiple instances of the singleton.
-        synchronized(this) {
-            val singletonKey = INST::class.java.name
+    inline fun <reified Dependency : Any> dependencyKey(): String = Dependency::class.java.name
 
-            return singletons[singletonKey] as? INST ?: newInstanceCreator().also {
-                singletons[singletonKey] = it
-            }
+    /**
+     * Internal function to get an overridden dependency in the graph.
+     * This function is marked as public only because it is inlined in other public functions.
+     * Do not call this function directly. Instead, use the `newInstance` or `singleton` functions
+     * to get the dependency.
+     */
+    inline fun <reified Dependency : Any> getOverriddenInstance(): Dependency? {
+        return overrides[dependencyKey<Dependency>()] as? Dependency
+    }
+
+    /**
+     * Internal function to get or create a singleton instance of a dependency in the graph.
+     * This function is thread-safe and will only create a single instance of the dependency.
+     * This function is marked as public only because it is inlined in other public functions.
+     * Do not call this function directly. Instead, use the `singleton` function.
+     */
+    inline fun <reified Dependency : Any> getOrCreateSingletonInstance(
+        newInstanceCreator: () -> Dependency
+    ): Dependency {
+        // Use a synchronized block to prevent multiple threads from creating multiple instances of the singleton.
+        synchronized(lock = singletons) {
+            val singletonKey = dependencyKey<Dependency>()
+            return singletons.getOrPut(singletonKey, newInstanceCreator) as Dependency
         }
     }
 
     /**
-     * Call to delete instances held by the graph. This is meant to be called in between automated tests but can also be called to reset that state of the SDK at runtime.
+     * Create new instance of dependency in the graph every time it is accessed.
+     * Example:
+     * ```
+     * val logger: Logger
+     *   get() = newInstance { LoggerImpl(...) }
+     * ```
+     */
+    inline fun <reified Dependency : Any> newInstance(
+        newInstanceCreator: () -> Dependency
+    ): Dependency = getOverriddenInstance() ?: newInstanceCreator()
+
+    /**
+     * Gets stored instance of dependency in the graph or null if it doesn't exist.
+     * Example:
+     * ```
+     * val logger: Logger?
+     *   get() = getOrNull()
+     * ```
+     */
+    inline fun <reified Dependency : Any> getOrNull(): Dependency? {
+        return getOverriddenInstance() ?: singletons[dependencyKey<Dependency>()] as? Dependency
+    }
+
+    /**
+     * Get or create a singleton instance of dependency in the graph.
+     * Example:
+     * ```
+     * val logger: Logger
+     *   get() = singleton { LoggerImpl(...) }
+     * ```
+     */
+    inline fun <reified Dependency : Any> singleton(newInstanceCreator: () -> Dependency): Dependency {
+        return getOverriddenInstance() ?: getOrCreateSingletonInstance(newInstanceCreator)
+    }
+
+    /**
+     * Overrides dependency with mock in test functions.
+     * `di.overrideDependency(CustomerIOInstance::class.java, customerIOMock)`
+     * See `CustomerIOTest` (or any other class that is top-level of the module) for an example on use.
+     */
+    fun <Dependency : Any> overrideDependency(dependency: Class<Dependency>, value: Dependency) {
+        overrides[dependency.name] = value as Any
+    }
+
+    /**
+     * Register a dependency in the graph. This is useful for dependencies that
+     * are provided later in the lifecycle of the application.
+     */
+    inline fun <reified Dependency : Any> registerDependency(newInstanceCreator: () -> Dependency): Dependency {
+        return getOrCreateSingletonInstance(newInstanceCreator)
+    }
+
+    /**
+     * Reset the graph to its initial state.
+     * This is meant to be called in between automated tests but can also be
+     * called to reset that state of the SDK at runtime.
      */
     fun reset() {
         overrides.clear()
         singletons.clear()
+    }
+
+    // TODO: Remove deprecated functions after all usages are removed.
+    @Deprecated("Use newInstance or singleton instead", ReplaceWith("newInstance()"))
+    inline fun <reified DEP : Any> override(): DEP? = overrides[dependencyKey<DEP>()] as? DEP
+
+    @Deprecated("Use singleton instead", ReplaceWith("singleton(newInstanceCreator)"))
+    inline fun <reified INST : Any> getSingletonInstanceCreate(newInstanceCreator: () -> INST): INST {
+        return getOrCreateSingletonInstance(newInstanceCreator)
     }
 }

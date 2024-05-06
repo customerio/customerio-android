eeb538cca918b1bd41a54c515e8a57b5794aba24
Rehan <mrehan.official@gmail.com>
2024-05-06 17:42:23 +0500

    fixed paths

--- core/src/main/kotlin/io/customer/sdk/android/CustomerIOInstance.kt
+++ core/src/main/kotlin/io/customer/sdk/android/CustomerIOInstance.kt
@@ -5,7 +5,7 @@
 package io.customer.sdk.android
 
 /**
- * Allows mocking of [CustomerIO] for automated tests in the project.
+ * Allows mocking of CustomerIO for automated tests in the project.
  * Mock the implementation of this interface to test the behavior of the SDK without actually calling the SDK.
  */
 interface CustomerIOInstance

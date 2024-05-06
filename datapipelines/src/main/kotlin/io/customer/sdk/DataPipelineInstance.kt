d050259f025cd7c3b9ec031305a42984d961f219
Rehan <mrehan.official@gmail.com>
2024-05-06 19:13:18 +0500

    added docs

--- datapipelines/src/main/kotlin/io/customer/sdk/DataPipelineInstance.kt
+++ datapipelines/src/main/kotlin/io/customer/sdk/DataPipelineInstance.kt
@@ -9,14 +9,61 @@ import kotlinx.serialization.SerializationStrategy
  * Data Pipelines implementation.
  */
 interface DataPipelineInstance : CustomerIOInstance {
+    /**
+     * Custom profile attributes to be added to current profile.
+     */
     var profileAttributes: CustomAttributes
+
+    /**
+     * Identify a customer (aka: Add or update a profile).
+     * [Learn more](https://customer.io/docs/identifying-people/) about identifying a customer in Customer.io
+     * Note: You can only identify 1 profile at a time in your SDK. If you call this function multiple times,
+     * the previously identified profile will be removed. Only the latest identified customer is persisted.
+     *
+     * @param userId ID you want to assign to the customer.
+     * This value can be an internal ID that your system uses or an email address.
+     * [Learn more](https://customer.io/docs/api/#operation/identify)
+     */
     fun identify(userId: String)
+
+    /**
+     * Identify a customer (aka: Add or update a profile).
+     * [Learn more](https://customer.io/docs/identifying-people/) about identifying a customer in Customer.io
+     * Note: You can only identify 1 profile at a time in your SDK. If you call this function multiple times,
+     * the previously identified profile will be removed. Only the latest identified customer is persisted.
+     *
+     * @param userId ID you want to assign to the customer.
+     * This value can be an internal ID that your system uses or an email address.
+     * [Learn more](https://customer.io/docs/api/#operation/identify)
+     * @param traits Map of <String, Any> to be added
+     */
     fun identify(userId: String, traits: CustomAttributes)
-    fun <Body> identify(
+
+    /**
+     * Identify a customer (aka: Add or update a profile).
+     * [Learn more](https://customer.io/docs/identifying-people/) about identifying a customer in Customer.io
+     * Note: You can only identify 1 profile at a time in your SDK. If you call this function multiple times,
+     * the previously identified profile will be removed. Only the latest identified customer is persisted.
+     *
+     * @param Traits Serializable json object to be added.
+     * @param userId ID you want to assign to the customer.
+     * This value can be an internal ID that your system uses or an email address.
+     * [Learn more](https://customer.io/docs/api/#operation/identify)
+     * @param traits [Traits] about the user. Needs to be [serializable](https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/serializers.md)
+     * @param serializationStrategy strategy to serialize [traits]
+     */
+    fun <Traits> identify(
         userId: String,
-        traits: Body,
-        serializationStrategy: SerializationStrategy<Body>
+        traits: Traits,
+        serializationStrategy: SerializationStrategy<Traits>
     )
 
+    /**
+     * Stop identifying the currently persisted customer. All future calls to the SDK will no longer
+     * be associated with the previously identified customer.
+     * Note: If you simply want to identify a *new* customer, this function call is optional. Simply
+     * call `identify()` again to identify the new customer profile over the existing.
+     * If no profile has been identified yet, this function will reset anonymous profile.
+     */
     fun clearIdentify()
 }

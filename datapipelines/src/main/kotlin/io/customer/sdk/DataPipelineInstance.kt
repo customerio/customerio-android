ea15d79c25db53de7920d1e27ec5e604b7901361
Rehan <mrehan.official@gmail.com>
2024-05-08 20:19:39 +0500

    doc updates

--- datapipelines/src/main/kotlin/io/customer/sdk/DataPipelineInstance.kt
+++ datapipelines/src/main/kotlin/io/customer/sdk/DataPipelineInstance.kt
@@ -18,6 +18,18 @@ abstract class DataPipelineInstance : CustomerIOInstance {
      */
     abstract var profileAttributes: CustomAttributes
 
+    /**
+     * Identify a customer (aka: Add or update a profile).
+     * [Learn more](https://customer.io/docs/identifying-people/) about identifying a customer in Customer.io
+     * Note: You can only identify 1 profile at a time in your SDK. If you call this function multiple times,
+     * the previously identified profile will be removed. Only the latest identified customer is persisted.
+     *
+     * @param Traits Serializable json object to be added.
+     * @param userId Identifier you want to assign to the customer.
+     * This value can be an internal ID that your system uses or an email address.
+     * [Learn more](https://customer.io/docs/api/#operation/identify)
+     * @param traits [Traits] about the user. Needs to be [serializable](https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/serializers.md)
+     */
     inline fun <reified Traits> identify(userId: String, traits: Traits) {
         identify(userId, traits, JsonAnySerializer.serializersModule.serializer())
     }
@@ -31,6 +43,7 @@ abstract class DataPipelineInstance : CustomerIOInstance {
      * @param userId Identifier you want to assign to the customer.
      * This value can be an internal ID that your system uses or an email address.
      * [Learn more](https://customer.io/docs/api/#operation/identify)
+     * @param traits JsonObject about the user.
      */
     @JvmOverloads
     fun identify(userId: String, traits: JsonObject = emptyJsonObject) {
@@ -43,7 +56,7 @@ abstract class DataPipelineInstance : CustomerIOInstance {
      * Note: You can only identify 1 profile at a time in your SDK. If you call this function multiple times,
      * the previously identified profile will be removed. Only the latest identified customer is persisted.
      *
-     * @param userId ID you want to assign to the customer.
+     * @param userId Identifier you want to assign to the customer.
      * This value can be an internal ID that your system uses or an email address.
      * [Learn more](https://customer.io/docs/api/#operation/identify)
      * @param traits Map of <String, Any> to be added
@@ -59,7 +72,7 @@ abstract class DataPipelineInstance : CustomerIOInstance {
      * the previously identified profile will be removed. Only the latest identified customer is persisted.
      *
      * @param Traits Serializable json object to be added.
-     * @param userId ID you want to assign to the customer.
+     * @param userId Identifier you want to assign to the customer.
      * This value can be an internal ID that your system uses or an email address.
      * [Learn more](https://customer.io/docs/api/#operation/identify)
      * @param traits [Traits] about the user. Needs to be [serializable](https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/serializers.md)

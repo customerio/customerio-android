43b4dfac2f2b55f9c6938d0bf1cef8cd51f749a2
Rehan <mrehan.official@gmail.com>
2024-05-14 00:24:57 +0500

    fixed doc

--- datapipelines/src/test/java/io/customer/datapipelines/extensions/DiGraphExt.kt
+++ datapipelines/src/test/java/io/customer/datapipelines/extensions/DiGraphExt.kt
@@ -8,7 +8,6 @@ import io.customer.sdk.core.extensions.registerDependency
 
 /**
  * Registers the analytics factory to the SDKComponent.
- * The key should match identifier defined in [SDKComponent.analyticsFactory].
  * The method is placed in test sources to avoid exposing it to release builds.
  */
 fun SDKComponent.registerAnalyticsFactory(

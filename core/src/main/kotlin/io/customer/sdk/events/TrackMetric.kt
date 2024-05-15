0135627e82fabf91d65a751cce081e8d49e75299
Rehan <mrehan.official@gmail.com>
2024-05-15 18:04:44 +0500

    added docs

--- core/src/main/kotlin/io/customer/sdk/events/TrackMetric.kt
+++ core/src/main/kotlin/io/customer/sdk/events/TrackMetric.kt
@@ -2,23 +2,34 @@ package io.customer.sdk.events
 
 import java.util.Locale
 
-enum class Metric {
-    Delivered,
-    Opened,
-    Converted,
-    Clicked
-}
-
+/**
+ * Event wrapper for tracking metrics like push, in-app, etc. with
+ * additional properties like deliveryId, deviceToken, metadata, etc.
+ */
 sealed interface TrackMetric {
     val metric: Metric
     val deliveryId: String
 
+    /**
+     * Event class for tracking push metrics
+     *
+     * @param metric Metric event to be tracked
+     * @param deliveryId Delivery ID of push notification
+     * @param deviceToken Device token to which the push notification was sent
+     */
     data class Push(
         override val metric: Metric,
         override val deliveryId: String,
         val deviceToken: String
     ) : TrackMetric
 
+    /**
+     * Event class for tracking in-app metrics
+     *
+     * @property metric Metric event to be tracked
+     * @property deliveryId Delivery ID of in-app message
+     * @property metadata Additional metadata for the in-app message
+     */
     data class InApp(
         override val metric: Metric,
         override val deliveryId: String,

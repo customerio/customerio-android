d218a52a50c2895c5738d114b27a5a8922678baf
Rehan <mrehan.official@gmail.com>
2024-06-13 16:26:03 +0500

    improved timeout exception

--- messaginginapp/src/androidTest/java/io/customer/messaginginapp/support/CoroutineExtensions.kt
+++ messaginginapp/src/androidTest/java/io/customer/messaginginapp/support/CoroutineExtensions.kt
@@ -1,16 +1,25 @@
 package io.customer.messaginginapp.support
 
+import kotlin.coroutines.cancellation.CancellationException
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
 
 private const val DEFERRED_WAIT_MAX_DELAY = 1_000L
 
 fun <T> Deferred<T>.awaitWithTimeoutBlocking(
     timeMillis: Long = DEFERRED_WAIT_MAX_DELAY
-): T = runBlocking { withTimeout(timeMillis) { await() } }
+): T = runBlocking {
+    withTimeoutOrNull(timeMillis) {
+        await()
+    } ?: throw CancellationException("Failed to await deferred within $timeMillis milliseconds.")
+}
 
 fun <T> Collection<Deferred<T>>.awaitWithTimeoutBlocking(
     timeMillis: Long = DEFERRED_WAIT_MAX_DELAY
-): List<T> = runBlocking { withTimeout(timeMillis) { awaitAll(*toTypedArray()) } }
+): List<T> = runBlocking {
+    withTimeoutOrNull(timeMillis) {
+        awaitAll(*toTypedArray())
+    } ?: throw CancellationException("Failed to await deferred within $timeMillis milliseconds.")
+}

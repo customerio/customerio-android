e017ee7dd71c3bc61d0e943b8338fd68677524b4
Rehan <mrehan.official@gmail.com>
2024-06-21 14:15:07 +0500

    chore: inapp config builder

--- messaginginapp/src/main/java/io/customer/messaginginapp/di/DIGraphMessagingInApp.kt
+++ messaginginapp/src/main/java/io/customer/messaginginapp/di/DIGraphMessagingInApp.kt
@@ -5,6 +5,7 @@ import io.customer.messaginginapp.provider.GistApi
 import io.customer.messaginginapp.provider.GistApiProvider
 import io.customer.messaginginapp.provider.GistInAppMessagesProvider
 import io.customer.messaginginapp.provider.InAppMessagesProvider
+import io.customer.sdk.android.CustomerIO
 import io.customer.sdk.core.di.SDKComponent
 
 internal val SDKComponent.gistApiProvider: GistApi
@@ -13,7 +14,10 @@ internal val SDKComponent.gistApiProvider: GistApi
 internal val SDKComponent.gistProvider: InAppMessagesProvider
     get() = newInstance<InAppMessagesProvider> { GistInAppMessagesProvider(gistApiProvider) }
 
-fun SDKComponent.inAppMessaging(): ModuleMessagingInApp {
-    return modules[ModuleMessagingInApp.moduleName] as? ModuleMessagingInApp
+// We need to add extension functions to the CustomerIO class as this is how
+// customers will interact with in-app messaging module.
+@Suppress("UnusedReceiverParameter")
+fun CustomerIO.inAppMessaging(): ModuleMessagingInApp {
+    return SDKComponent.modules[ModuleMessagingInApp.MODULE_NAME] as? ModuleMessagingInApp
         ?: throw IllegalStateException("ModuleMessagingInApp not initialized")
 }

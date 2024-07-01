2dedbfd4509aa90ddd9f903ecf39fb6baf374575
Rehan <mrehan.official@gmail.com>
2024-07-01 15:54:35 +0500

    removed sharedTest

---
+++ sdk/src/androidTest/java/io/customer/sdk/util/commontest/DateUtilStub.kt
@@ -0,0 +1,20 @@
+package io.customer.sdk.util.commontest
+
+import io.customer.base.extenstions.getUnixTimestamp
+import io.customer.sdk.util.DateUtil
+import java.util.*
+
+/**
+ * Convenient alternative to mocking [DateUtil] in your test since the code is boilerplate.
+ */
+class DateUtilStub : DateUtil {
+
+    // modify this value in your test class if you need to.
+    var givenDate: Date = Date(1646238885L)
+
+    override val now: Date
+        get() = givenDate
+
+    override val nowUnixTimestamp: Long
+        get() = now.getUnixTimestamp()
+}

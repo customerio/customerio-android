d44c91b39a083d8a9a7865250ae91b58614dfb3e
Muhammad Rehan <mrehan.official@gmail.com>
2024-07-03 13:30:52 +0500

    refactor: common-test base test classes (#374)

---
+++ common-test/src/main/java/io/customer/commontest/config/TestConfig.kt
@@ -0,0 +1,42 @@
+package io.customer.commontest.config
+
+/**
+ * Base interface for all test configurations.
+ * A test configuration is a collection of test arguments and a DI graph configuration.
+ * This allows for easy configuration of test environments and dependencies.
+ * The configuration can be combined with other configurations using the [plus] operator
+ * to create a new configuration that contains all the arguments and DI graph configurations of both.
+ */
+interface TestConfig {
+    val arguments: List<TestArgument>
+    val diGraph: DIGraphConfiguration
+
+    /**
+     * Combines this configuration with another configuration to create a new configuration.
+     * The new configuration contains all the arguments and DI graph configurations of both configurations.
+     * Configuration values from the other configuration take precedence over this configuration.
+     * So in general, child configurations should be passed as the other configuration to override parent configurations.
+     * However, in case of DSL configurations, the parent configuration is called first to mimic the behavior of overriding.
+     */
+    operator fun plus(other: TestConfig): TestConfig
+}
+
+/**
+ * Builder class for creating test configurations using a DSL.
+ * The builder is responsible for creating a new configuration instance with the provided arguments
+ * using DSL functions and returning configuration instance.
+ */
+abstract class TestConfigBuilder<Config : TestConfig> {
+    abstract fun build(): Config
+
+    protected var configArguments: MutableList<TestArgument> = mutableListOf()
+    protected var diGraphConfiguration: DIGraphConfiguration = DIGraphConfiguration()
+
+    fun argument(value: TestArgument) {
+        configArguments.add(value)
+    }
+
+    fun diGraph(block: ConfigDSL<DIGraphConfiguration>) {
+        diGraphConfiguration = DIGraphConfiguration().apply(block)
+    }
+}

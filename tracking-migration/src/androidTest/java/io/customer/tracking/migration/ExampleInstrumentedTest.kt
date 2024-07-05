cc6c62912c77c4563fb06bce4d1a1b8af5707012
Shahroz Khan <shrozali@gmail.com>
2024-07-05 12:14:31 -0400

    pr suggestions

--- tracking-migration/src/androidTest/java/io/customer/tracking/migration/ExampleInstrumentedTest.kt
+++ tracking-migration/src/androidTest/java/io/customer/tracking/migration/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package io.customer.migration
+package io.customer.tracking.migration
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.customer.migration.test", appContext.packageName)
+        assertEquals("io.customer.tracking.migration.test", appContext.packageName)
     }
 }
--- tracking-migration/src/test/java/io/customer/tracking/migration/ExampleUnitTest.kt
+++ tracking-migration/src/test/java/io/customer/tracking/migration/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package io.customer.migration
+package io.customer.tracking.migration
 
 import org.junit.Assert.*
 import org.junit.Test

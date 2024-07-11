a8e34207b892d335496a67170778a547e7a2c145
Shahroz Khan <shrozali@gmail.com>
2024-07-11 10:51:41 -0400

    chore: update queue to remove unused logic (#386)

--- tracking-migration/src/main/java/io/customer/tracking/migration/queue/QueueQueryRunner.kt
+++ tracking-migration/src/main/java/io/customer/tracking/migration/queue/QueueQueryRunner.kt
@@ -4,60 +4,19 @@ import io.customer.sdk.core.util.Logger
 import io.customer.tracking.migration.type.QueueTaskMetadata
 
 interface QueueQueryRunner {
-    fun getNextTask(queue: List<QueueTaskMetadata>, lastFailedTask: QueueTaskMetadata?): QueueTaskMetadata?
-    fun reset()
+    fun getNextTask(queue: List<QueueTaskMetadata>): QueueTaskMetadata?
 }
 
 internal class QueueQueryRunnerImpl(
     private val logger: Logger
 ) : QueueQueryRunner {
-    internal val queryCriteria = QueueQueryCriteria()
 
-    override fun getNextTask(queue: List<QueueTaskMetadata>, lastFailedTask: QueueTaskMetadata?): QueueTaskMetadata? {
+    override fun getNextTask(queue: List<QueueTaskMetadata>): QueueTaskMetadata? {
         if (queue.isEmpty()) return null
-        if (lastFailedTask != null) updateCriteria(lastFailedTask)
 
         // log *after* updating the criteria
-        logger.debug("queue querying next task. criteria: $queryCriteria")
+        logger.debug("queue querying next task")
 
-        return queue.firstOrNull { doesTaskPassCriteria(it) }
-    }
-
-    internal fun updateCriteria(lastFailedTask: QueueTaskMetadata) {
-        lastFailedTask.groupStart?.let { queueGroupName ->
-            queryCriteria.excludeGroups.add(queueGroupName)
-        }
-    }
-
-    private fun doesTaskPassCriteria(task: QueueTaskMetadata): Boolean {
-        // At this time, function only contains 1 query criteria. If more were added in the future, we can chain them together:
-        // queryCriteria.doesTaskX() ?: queryCriteria.doesTaskY()
-        return !doesTaskBelongToExcludedGroup(task)
-    }
-
-    private fun doesTaskBelongToExcludedGroup(task: QueueTaskMetadata): Boolean {
-        task.groupMember?.let { groupsTaskBelongsTo ->
-            queryCriteria.excludeGroups.forEach { groupToExclude ->
-                if (groupsTaskBelongsTo.contains(groupToExclude)) {
-                    return true
-                }
-            }
-        }
-
-        return false
-    }
-
-    override fun reset() {
-        logger.debug("resetting queue tasks query criteria")
-
-        queryCriteria.reset()
-    }
-
-    internal data class QueueQueryCriteria(
-        val excludeGroups: MutableSet<String> = mutableSetOf()
-    ) {
-        fun reset() {
-            excludeGroups.clear()
-        }
+        return queue.firstOrNull()
     }
 }

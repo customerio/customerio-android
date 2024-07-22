c4684f420fe31c7994e541a4a89cd8fff3def427
Rehan <mrehan.official@gmail.com>
2024-07-22 12:52:43 +0500

    pr suggestions

--- common-test/src/main/java/io/customer/commontest/extensions/AssertionExtensions.kt
+++ common-test/src/main/java/io/customer/commontest/extensions/AssertionExtensions.kt
@@ -4,16 +4,32 @@ package io.customer.commontest.extensions
  * Extension function to assert that Result is failure and the exception is of the specified type.
  */
 inline fun <reified E : Throwable> Result<Any>.shouldBeFailure(): E {
-    if (!isFailure) {
-        throw AssertionError("Expected failure but the result was successful")
+    assertCondition(isFailure) {
+        "Expected failure but the result was successful"
     }
 
-    val exception = exceptionOrNull()
-    if (exception == null) {
-        throw AssertionError("Expected exception of type ${E::class} but was null")
-    } else if (exception !is E) {
-        throw AssertionError("Expected exception of type ${E::class} but was ${exception::class}")
+    return assertNotNull(exceptionOrNull() as? E) {
+        "Expected exception of type ${E::class} but was ${exceptionOrNull()?.let { it::class } ?: "null"}"
     }
+}
 
-    return exception
+/**
+ * Extension function to assert that Result is success and return the value.
+ * If the result is failure, it will throw an AssertionError.
+ */
+inline fun assertCondition(condition: Boolean, lazyMessage: () -> String) {
+    check(condition) {
+        throw AssertionError(lazyMessage())
+    }
+}
+
+/**
+ * Extension function to assert that the value is not null and return it.
+ * If the value is null, it will throw an AssertionError.
+ */
+inline fun <T : Any> assertNotNull(value: T?, lazyMessage: () -> String): T {
+    check(value != null) {
+        throw AssertionError(lazyMessage())
+    }
+    return value
 }

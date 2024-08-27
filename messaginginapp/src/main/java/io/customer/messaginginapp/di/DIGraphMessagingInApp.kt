61ab6cdaf761185ffa924dc4a5c26a8b64282579
Shahroz Khan <shrozali@gmail.com>
2024-08-27 14:57:47 +0500

    chore: refactor Gist SDK interfacing (#427)

--- messaginginapp/src/main/java/io/customer/messaginginapp/di/DIGraphMessagingInApp.kt
+++ messaginginapp/src/main/java/io/customer/messaginginapp/di/DIGraphMessagingInApp.kt
@@ -1,24 +1,38 @@
 package io.customer.messaginginapp.di
 
+import io.customer.messaginginapp.MessagingInAppModuleConfig
 import io.customer.messaginginapp.ModuleMessagingInApp
-import io.customer.messaginginapp.provider.GistApi
-import io.customer.messaginginapp.provider.GistApiProvider
-import io.customer.messaginginapp.provider.GistInAppMessagesProvider
-import io.customer.messaginginapp.provider.InAppMessagesProvider
+import io.customer.messaginginapp.gist.data.listeners.GistQueue
+import io.customer.messaginginapp.gist.data.listeners.Queue
+import io.customer.messaginginapp.gist.presentation.GistProvider
+import io.customer.messaginginapp.gist.presentation.GistSdk
 import io.customer.messaginginapp.state.InAppMessagingManager
 import io.customer.sdk.CustomerIOInstance
 import io.customer.sdk.core.di.SDKComponent
 
-internal val SDKComponent.gistApiProvider: GistApi
-    get() = newInstance<GistApi> { GistApiProvider() }
+internal val SDKComponent.gistQueue: GistQueue
+    get() = singleton<GistQueue> { Queue() }
 
-internal val SDKComponent.gistProvider: InAppMessagesProvider
-    get() = newInstance<InAppMessagesProvider> { GistInAppMessagesProvider(gistApiProvider) }
+internal val SDKComponent.moduleConfig: MessagingInAppModuleConfig
+    get() = inAppMessaging.moduleConfig
 
-@Suppress("UnusedReceiverParameter")
-fun CustomerIOInstance.inAppMessaging(): ModuleMessagingInApp {
-    return ModuleMessagingInApp.instance()
-}
-
+internal val SDKComponent.gistProvider: GistProvider
+    get() = singleton<GistProvider> {
+        GistSdk(
+            application = android().application,
+            siteId = moduleConfig.siteId,
+            dataCenter = moduleConfig.region.code
+        )
+    }
 internal val SDKComponent.inAppMessagingManager: InAppMessagingManager
-    get() = singleton<InAppMessagingManager> { InAppMessagingManager }
+    get() = singleton<InAppMessagingManager> { InAppMessagingManager(inAppMessaging) }
+
+/**
+ * Get the [ModuleMessagingInApp] instance from the [CustomerIOInstance]
+ * needed for the in-app messaging dismiss() method
+ */
+@Suppress("UnusedReceiverParameter")
+fun CustomerIOInstance.inAppMessaging(): ModuleMessagingInApp = SDKComponent.inAppMessaging
+
+internal val SDKComponent.inAppMessaging: ModuleMessagingInApp
+    get() = ModuleMessagingInApp.instance()

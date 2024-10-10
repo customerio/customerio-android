fec7c4fcdd813e1cd1628b92ef843c86f7e8652a
Mahmoud Elmorabea <mahmoud.elmorabea@gmail.com>
2024-10-10 22:27:32 +0300

    Implement internal settings screen activity

--- samples/java_layout/src/main/java/io/customer/android/sample/java_layout/utils/ViewUtils.java
+++ samples/java_layout/src/main/java/io/customer/android/sample/java_layout/utils/ViewUtils.java
@@ -12,6 +12,7 @@ import androidx.annotation.StringRes;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Locale;
@@ -69,5 +70,16 @@ public class ViewUtils {
         return new MaterialAlertDialogBuilder(activity)
                 .setCancelable(true)
                 .setPositiveButton(android.R.string.ok, null);
+    }
+
+    public static void clearErrorWhenTextedEntered(@NonNull TextInputEditText editText,
+                                                   @NonNull TextInputLayout textInputLayout) {
+        editText.addTextChangedListener(new DefaultTextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textInputLayout.setError(null);
+                textInputLayout.setErrorEnabled(false);
+            }
+        });
     }
 }

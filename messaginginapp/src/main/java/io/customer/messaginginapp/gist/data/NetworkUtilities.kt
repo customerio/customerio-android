26e63ea6be1de1973f2653e4c367dcb6dfa921d9
Shahroz Khan <shrozali@gmail.com>
2024-11-05 01:05:45 +0500

    added version and agent

--- messaginginapp/src/main/java/io/customer/messaginginapp/gist/data/NetworkUtilities.kt
+++ messaginginapp/src/main/java/io/customer/messaginginapp/gist/data/NetworkUtilities.kt
@@ -5,5 +5,7 @@ class NetworkUtilities {
         internal const val CIO_SITE_ID_HEADER = "X-CIO-Site-Id"
         internal const val USER_TOKEN_HEADER = "X-Gist-Encoded-User-Token"
         internal const val CIO_DATACENTER_HEADER = "X-CIO-Datacenter"
+        internal const val CIO_CLIENT_PLATFORM = "X-CIO-Client-Platform"
+        internal const val CIO_CLIENT_VERSION = "X-CIO-Client-Version"
     }
 }

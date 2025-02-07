74eaf253dfc7b1c366453d1d1ae948f0f7e8cc53
Rehan <mrehan.official@gmail.com>
2025-02-07 18:41:59 +0500

    added commit count

--- samples/java_layout/src/main/java/io/customer/android/sample/java_layout/buildinfo/BuildInfoMetadata.java
+++ samples/java_layout/src/main/java/io/customer/android/sample/java_layout/buildinfo/BuildInfoMetadata.java
@@ -27,8 +27,10 @@ public class BuildInfoMetadata {
     private final String sdkIntegration;
 
     public BuildInfoMetadata() {
-        this.sdkVersion = BuildInfoMetadataUtils.resolveValidOrElse(BuildConfig.SDK_VERSION, () -> Version.version + " (as source)");
-        this.appVersion = BuildConfig.VERSION_NAME;
+        this.sdkVersion = String.format(Locale.ENGLISH, "%s-%s",
+                BuildInfoMetadataUtils.resolveValidOrElse(BuildConfig.SDK_VERSION, () -> Version.version),
+                BuildInfoMetadataUtils.resolveValidOrElse(BuildConfig.COMMITS_AHEAD_COUNT, () -> "as-source"));
+        this.appVersion = String.valueOf(BuildConfig.VERSION_CODE);
         this.buildDate = BuildInfoMetadataUtils.formatBuildDateWithRelativeTime(BuildConfig.BUILD_TIMESTAMP);
         this.gitMetadata = String.format(Locale.ENGLISH, "%s-%s",
                 BuildInfoMetadataUtils.resolveValidOrElse(BuildConfig.BRANCH_NAME, () -> "working branch"),

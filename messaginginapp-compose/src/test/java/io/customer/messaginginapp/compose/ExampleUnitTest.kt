0504755df4a916ad2a527f3c8eaa58888dc2f220
Shahroz Khan <shrozali@gmail.com>
2025-05-16 16:13:12 +0500

    binary scale

---
+++ messaginginapp-compose/src/test/java/io/customer/messaginginapp/compose/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package io.customer.messaginginapp.compose
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}

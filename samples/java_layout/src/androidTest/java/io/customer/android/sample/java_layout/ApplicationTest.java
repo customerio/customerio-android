189dd2825d680e1732d5d104266622c64a5cee11
Shahroz Khan <shrozali@gmail.com>
2025-07-31 08:57:41 -0400

    chore: automatee strict mode violation test (#588)

---
+++ samples/java_layout/src/androidTest/java/io/customer/android/sample/java_layout/ApplicationTest.java
@@ -0,0 +1,13 @@
+package io.customer.android.sample.java_layout;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+    @Test
+    public void testApp() {
+    }
+}
